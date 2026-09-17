b6a4dea32a29d18f384bf913c588fcb48ba05902
MD. SAMIUR RAHMAN TANIM <codewithtanim+support@gmail.com>
2026-09-17 15:13:59 +0530

    Footer Added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import logo from "../assets/movieExplorerLogo.png";
+
+export default function Footer() {
+  return (
+    <footer className="border-t border-white/10 bg-[#B9F8CF]">
+      <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-5 px-6 py-8 md:flex-row">
+        {/* Logo || Name */}
+        <div>
+          <img src={logo} alt="MovieExplorer Logo" className="h-12 w-auto" />
+          {/* Copyright */}
+          <p className="mt-2 text-sm text-black-400">
+            © 2026 MovieExplorer. All rights reserved.
+          </p>
+        </div>
+
+        {/* Social Links */}
+        <div className="flex items-center gap-6">
+          {/* Facebook */}
+          <a
+            href="#"
+            className="text-sm text-black-400 transition hover:text-white"
+          >
+            Facebook
+          </a>
+          {/* Instagram */}
+          <a
+            href="#"
+            className="text-sm text-black-400 transition hover:text-white"
+          >
+            Instagram
+          </a>
+          {/* Twitter */}
+          <a
+            href="#"
+            className="text-sm text-black-400 transition hover:text-white"
+          >
+            Twitter
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar.jsx";
 import Hero from "../components/Hero.jsx";
+import Footer from "../components/Footer.jsx";
 
 
 export default function Home() {
@@ -9,6 +10,8 @@ export default function Home() {
       <Navbar />
       {/* Hero-Section */}
       <Hero />
+      {/* Footer */}
+      <Footer />
     </div>
   )
 }
